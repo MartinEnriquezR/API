366e50888abbb6beb27b7d1d28697044fa768a6a
Martin Enriquez <jmeenriquezr@gmail.com>
2021-07-18 18:57:37 -0500

    Cambio zona horaria

--- main/settings.py
+++ main/settings.py
@@ -160,7 +160,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Mexico_City'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
